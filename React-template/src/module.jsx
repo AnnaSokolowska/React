e4671eeb21f63076aba067543ce7b481549cbed6
Anna Sokolowska <milanka.anka@gmail.com>
2022-10-27 17:59:59 +0200

    react

---
+++ React-template/src/module.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import paint from './img/mountain_landscape-wallpaper-1920x1200.jpg';
+
+
+const Module = () => (
+    <>
+        <h1>Hello, React!</h1>
+        <img width={500} src={paint} />
+    </>
+)
+
+export default Module;
